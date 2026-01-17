372ab1afbab7fda5e41385ede814fadc37fec8bb
Mallachi <Lars.erik.vaagen@gmail.com>
2026-01-17 22:23:55 +0100

    updated version number.

--- version_checker.py
+++ version_checker.py
@@ -17,7 +17,7 @@ except ImportError:
 
 
 # Current version - update this with each release
-CURRENT_VERSION = "3.1.2"
+CURRENT_VERSION = "3.1.3"
 
 # GitHub repository info - UPDATE THESE when repo is created
 GITHUB_OWNER = "Diftic"
